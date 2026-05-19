f49b421a1d376354119c67372a2e397c1caa45ac
Igor Oliveira <131825435+IgorOlive11@users.noreply.github.com>
2026-05-19 21:21:18 +0000

    feat: auth flow working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useAuth } from "./hooks/useAuth";
+
+
 export default function App() {
+  const { authUser, authLoading } = useAuth()
+
+  if (authLoading) return <div>Carregando...</div>
+  if (!authUser) return <div>Login</div>
+
   return (
     <div>
       <h1>Overload</h1>
     </div>
-  );
+  )
 }
